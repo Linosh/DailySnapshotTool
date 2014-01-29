58d918072febbde2094b1a823e3027840b207a7e
Linosh <linoshmail@gmail.com>
2014-01-29 11:17:45 +0200

    Add websockets test server endpoint and eventbus

--- src/main/java/com/dst/web/websocket/EchoEndpointAnnotated.java
+++ src/main/java/com/dst/web/websocket/EchoEndpointAnnotated.java
@@ -1,8 +1,11 @@
 package com.dst.web.websocket;
 
-import javax.websocket.OnMessage;
-import javax.websocket.Session;
+import com.dst.web.eventbus.EventBusContext;
+import com.google.common.eventbus.Subscribe;
+
+import javax.websocket.*;
 import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
 
 /**
  * Project: DailySnapshotTool
@@ -11,8 +14,32 @@ import javax.websocket.server.ServerEndpoint;
  */
 @ServerEndpoint(value = "/echo")
 public class EchoEndpointAnnotated {
+	private Session session;
+
+	@OnOpen
+	public void onOpen(Session session) throws IOException {
+		this.session = session;
+		EventBusContext.registerListener(this);
+		session.getBasicRemote().sendText("Connection opened");
+	}
+
+	@Subscribe
+	public void sendMessage(String msg) throws IOException{
+		this.session.getBasicRemote().sendText(msg);
+	}
+
 	@OnMessage
-	public String onMessage(String message, Session session) {
-		return message;
+	public String echo(String message) {
+		return message + " (from your server)";
+	}
+
+	@OnError
+	public void onError(Throwable t) {
+		t.printStackTrace();
+	}
+
+	@OnClose
+	public void onClose(Session session) {
+		EventBusContext.unregisterListener(this);
 	}
 }
